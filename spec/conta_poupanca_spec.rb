f59a2b3881e19383132c667e5bebde80e119c81e
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-07-02 15:58:05 -0300

    Add conta corrente and conta poupanca specs

---
+++ spec/conta_poupanca_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe ContaPoupanca do
+  it "deveria não cobrar taxa no deposito" do
+    pending
+    conta = conta_poupanca
+    saldo_anterior = conta.saldo
+    valor_deposito = 200
+    conta.depositar(valor_deposito)
+    expect(conta.saldo).to eq(saldo_anterior + valor_deposito)
+  end
+
+  it "deveria adicionar juros quando atualizada" do
+    pending
+    conta = conta_poupanca
+    saldo_anterior = conta.saldo
+    juros = 0.005
+    conta.atualizar(juros)
+    expect(conta.saldo).to eq(saldo_anterior * (1 + juros))
+  end
+
+  def conta_poupanca
+    ContaPoupanca.new(numero: "5354-4500", titular: Cliente.new("Jose da Silva"), saldo: 1000.10)
+  end
+end
