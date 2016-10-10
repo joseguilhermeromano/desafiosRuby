e71663d3a6e5f3253542013f7c8ed1838f65a84e
Alan Rafael R. Batista <alanrrb@gmail.com>
2016-10-09 21:17:37 -0300

    Initial Commit

---
+++ lib/tipos_de_dados.rb
@@ -0,0 +1,33 @@
+class TiposDeDados
+  def ola_mundo
+
+  end
+
+  def um_numero
+
+  end
+
+  def um_decimal
+
+  end
+
+  def verdadeiro?
+
+  end
+
+  def falso?
+
+  end
+
+  def array_vazio
+
+  end
+
+  def array_com_um_elemento
+
+  end
+
+  def array_com_dois_elementos
+
+  end
+end
