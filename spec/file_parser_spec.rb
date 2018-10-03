6759c3465a821ff95411f7d61f6db6608c0d944e
Henrique Morato <henrique.morato@campuscode.com.br>
2018-10-03 10:54:40 -0300

    Tests update

--- spec/file_parser_spec.rb
+++ spec/file_parser_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
 
 describe FileParser do
-  let(:file_parser) { FileParser.new }
-
   it 'converte string de números para arquivo de binários' do
-    file_parser.to_binary_file('100;200;300;400')
+    FileParser.new.to_binary_file('100;200;300;400')
     expect(File.read('data/data.txt')).to include(
       "1100100\n11001000\n100101100\n110010000")
   end
 
   it 'converte string de números para arquivo de binários' do
-    file_parser.to_binary_file('299;300;301;302')
+    FileParser.new.to_binary_file('299;300;301;302')
     expect(File.read('data/data.txt')).to include(
       "100101011\n100101100\n100101101\n100101110")
   end
