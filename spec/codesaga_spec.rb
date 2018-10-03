83310b953e1981185266cf9128bdf5019ee2290c
Henrique Morato <henrique.morato@campuscode.com.br>
2018-10-03 11:06:42 -0300

    Tests update

--- spec/codesaga_spec.rb
+++ spec/codesaga_spec.rb
@@ -1,24 +1,22 @@
 require 'spec_helper'
 
 describe CodeSaga do
-  let(:code_saga) { CodeSaga.new }
-
   it 'números divisiveis por 3 trocar por Code até o valor 3' do
-    expect(code_saga.code_or_saga(3)).to eq [1, 2, 'Code']
+    expect(CodeSaga.new.code_or_saga(3)).to eq [1, 2, 'Code']
   end
 
   it 'números divisiveis por 5 trocar por Saga até o valor 5' do
-    expect(code_saga.code_or_saga(5)).to eq [1, 2, 'Code', 4, 'Saga']
+    expect(CodeSaga.new.code_or_saga(5)).to eq [1, 2, 'Code', 4, 'Saga']
   end
 
   it 'Code e Saga até 10' do
-    expect(code_saga.code_or_saga(10)).to eq(
+    expect(CodeSaga.new.code_or_saga(10)).to eq(
       [1, 2, 'Code', 4, 'Saga', 'Code', 7, 8, 'Code', 'Saga']
     )
   end
 
   it 'Code e Saga até 100' do
-    expect(code_saga.code_or_saga(100)).to eq(
+    expect(CodeSaga.new.code_or_saga(100)).to eq(
       [1, 2, 'Code', 4, 'Saga', 'Code', 7, 8, 'Code', 'Saga', 11, 'Code',
        13, 14, 'CodeSaga', 16, 17, 'Code', 19, 'Saga', 'Code', 22, 23, 'Code',
        'Saga', 26, 'Code', 28, 29, 'CodeSaga', 31, 32, 'Code', 34, 'Saga',
