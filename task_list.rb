e90cbd8e7dbe4efc1d941e76c7fb9d4ec7788b89
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-09 00:59:27 -0300

    Implementação do exercício de hashes

--- task_list.rb
+++ task_list.rb
@@ -1,15 +1,21 @@
 def menu()
-  puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+  puts "--- Menu ---"
+  puts
+  puts "Bem-vindo ao Task List! Escolha uma opção no menu:"
+  puts
   puts '[1] Inserir uma tarefa'
   puts '[2] Ver todas as tarefas'
   puts '[3] Buscar Tarefa'
-  puts '[4] Sair'
+  puts '[4] Mudar o status de uma tarefa'
+  puts '[5] Sair'
   puts
   print 'Opção escolhida: '
   gets.to_i
 end
 
 def busca(tarefas)
+  puts "--- Buscar Tarefa ---"
+  puts
   print 'Digite sua busca: '
   busca = gets.strip
   #tarefas = ['comprar pão', 'comprar leite', 'fazer bolo']
@@ -22,33 +28,62 @@ def busca(tarefas)
   return elementos_encontrados
 end
 
+def add_tarefa(tarefas)
+  puts "--- Adicionar Tarefa ---"
+  puts 
+  print 'Digite sua tarefa: '
+  tarefa = Hash.new
+  tarefa = {tarefa: gets.strip, status: false}
+  tarefas.push(tarefa)
+  puts
+  puts 'Tarefa cadastrada: ' + tarefas.last[:tarefa] + ' Status: ' + 
+  (tarefas.last[:status] == false && 'Tarefa ainda não realizada!')
+  puts 
+end
+
+def exibir_tarefas(tarefas)
+  puts "--- Lista de Tarefas ---"
+  puts
+	tarefas.each_with_index do |tarefa, index|
+    puts ' - ' + "##{ index } - Tarefa:  #{ tarefa[:tarefa] } " +
+    " - Status: #{ tarefa[:status] ||= 'Tarefa ainda não concluída!' || 
+    tarefa[:status] &&= 'Tarefa concluída!' }"
+	end
+  puts
+  puts
+end
+
+def concluir_tarefa(tarefas)
+  puts "--- Concluir Tarefa ---"
+  puts 
+  exibir_tarefas(tarefas)
+  print 'Digite o código da tarefa: '
+  indice = gets.to_i
+  tarefas[indice][:status] = true
+  puts 
+  puts "Tarefa atualizada: " + tarefas[indice][:tarefa] + " Status: " + 
+  (tarefas[indice][:status] &&= "Tarefa concluída!")
+  puts
+end
+
 
 opcao=0
 tarefas = []
-while opcao != 4 do
+while opcao != 5 do
   
   opcao = menu()
   system('clear')
   case opcao
   when 1
-    print 'Digite sua tarefa: '
-    tarefas.push(gets.strip)
-    puts
-    puts 'Tarefa cadastrada: ' + tarefas . last
+    add_tarefa(tarefas)
   when 2
-    puts
-    #puts tarefas
-	j = 0
-	tarefas.each_with_index do |tarefa, index|
-	j += 10
-	  #redo unless index % 10 == 0
-	  print ' - ' + "##{ j + index } - #{ tarefa }" . strip
-	end
-	puts
+    exibir_tarefas(tarefas)
   when 3
     puts
     puts "Resultado da busca: #{busca(tarefas)}"
   when 4
+    concluir_tarefa(tarefas)
+  when 5
     puts 'Programa encerrado...'
   else
     puts
