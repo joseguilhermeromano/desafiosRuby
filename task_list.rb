de65295090569f0c1dac9111f5c086afa0a2930b
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-08 21:06:17 -0300

    Saga primeiros passos com ruby, commit relacionado aos desafios: #1 iniciando o projeto: Estruturas de repetição, métodos, Arrays e #2 Trabalhando com coleções: Listar elementos, buscar numa coleção!

---
+++ task_list.rb
@@ -0,0 +1,61 @@
+def menu()
+  puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+  puts '[1] Inserir uma tarefa'
+  puts '[2] Ver todas as tarefas'
+  puts '[3] Buscar Tarefa'
+  puts '[4] Sair'
+  puts
+  print 'Opção escolhida: '
+  gets.to_i
+end
+
+def busca(tarefas)
+  puts 'Digite sua busca: '
+  busca = gets.strip
+  #tarefas = ['comprar pão', 'comprar leite', 'fazer bolo']
+  elementos_encontrados = []
+  tarefas.each do |tarefa|
+      my_string = "abcdefg"
+	if my_string.include? "cde"
+	   puts "String includes 'cde'"
+	end
+#    if tarefa.include? busca == true
+#      elementos_encontrados << tarefa
+#    end
+  end
+end
+
+
+opcao=0
+tarefas = []
+while opcao != 4 do
+  
+  opcao = menu()
+  system('clear')
+  case opcao
+  when 1
+    print 'Digite sua tarefa: '
+    tarefas.push(gets)
+    puts
+    puts 'Tarefa cadastrada: ' + tarefas . last
+  when 2
+    puts
+    #puts tarefas
+	j = 0
+	tarefas.each_with_index do |tarefa, index|
+	j += 10
+	  #redo unless index % 10 == 0
+	  print ' - ' + "##{ j + index } - #{ tarefa }" . strip
+	end
+	puts
+  when 3
+    elementos_encontrados = busca(tarefas)
+    puts elementos_encontrados
+  when 4
+    puts 'Programa encerrado...'
+  else
+    puts
+    puts 'Opção inválida!'
+  end
+  
+end
