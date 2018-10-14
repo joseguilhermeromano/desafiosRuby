fec135e652da86fbf5bbbab297a6720cc1df44e7
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-14 02:23:20 +0000

    Finalização da solução!

--- lib/echo.rb
+++ lib/echo.rb
@@ -1,2 +1,13 @@
 class Echo
+  def message(frase)
+    if frase.gsub(/\s+/, "").empty?
+      "Nada?"
+    elsif frase[-1] == "?"
+      "Só faço echo, não respondo perguntas."
+    elsif frase =~ /^[ \p{Uppercase} [:punct:]]+$/
+      "Não faço echo de gritos."
+    else
+      frase
+    end
+  end
 end
