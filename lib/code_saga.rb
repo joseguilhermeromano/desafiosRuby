b3e0419e3a9568ebd8cce82070612a48e2f0d738
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-14 16:19:25 +0000

    Finalização do desafio Code ou Saga!

--- lib/code_saga.rb
+++ lib/code_saga.rb
@@ -1,2 +1,12 @@
 class CodeSaga
+  def code_or_saga(number)
+    array = []
+    1.upto(number) do |n|
+      next array << "CodeSaga" if (n % 3 == 0 and n % 5 == 0)
+      next array << "Code" if n % 3 == 0 
+      next array << "Saga" if n % 5 == 0 
+      array << n
+    end
+    array
+  end
 end
