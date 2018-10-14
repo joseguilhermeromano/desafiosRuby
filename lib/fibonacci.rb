a7f2f732750ca76ea27b215bb0de46397e80ac77
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-14 15:31:16 +0000

    Finalizando solução do desafio fibonacci!

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,2 +1,18 @@
 class Fibonacci
+  
+  def element(n)
+    if n <= 1
+      return n
+    end
+    return element(n-1) + element(n-2)
+  end
+
+  def elements(n)
+    elements = []
+    0.upto(n) do |number|
+      elements << element(number)
+    end
+    elements
+  end
+
 end
