9d7ee2372437c370529995d027dd8e2ab68be7be
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-16 00:21:28 +0000

    Finalização do desafio

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,2 +1,26 @@
 class RomanNumerals
+  def from_decimal(number)
+    roman_numerals = 
+    { "M"  => 1000,
+      "CM" => 900, 
+      "D"  => 500,
+      "CD" => 400, 
+      "C"  => 100,
+      "XC" => 90,
+      "L"  => 50, 
+      "XL" => 40,
+      "X"  => 10,
+      "IX" => 9,
+      "V"  => 5,
+      "IV" => 4,
+      "I"  => 1 }
+    convertion = ""
+
+    roman_numerals.each do |roman, value|
+      part = (number / value)
+      convertion += roman * part
+      number -= part * value 
+    end
+    convertion
+  end
 end
