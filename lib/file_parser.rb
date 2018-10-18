b2ec5981eaa70e547b4609bc3e077c5121b058bc
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-18 04:11:03 +0000

    Finalização do desafio!

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,2 +1,18 @@
 class FileParser
+  def to_binary(decimal)
+    decimal.to_i.to_s(2)
+  end
+
+  def to_binary_file(decimal_numbers)
+    file = "data/data.txt"
+    binarys = decimal_numbers.split(";").map{|e| to_binary(e)}
+
+    if not File.exist? file
+       File.new file
+    end
+
+    File.open(file, 'w+') do |f|
+        f.puts binarys
+    end
+  end
 end
