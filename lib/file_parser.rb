7e2d30c3cede421f3d3111ce98ad9a295de8466e
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-18 02:12:01 +0000

    Finalização do desafio

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,2 +1,24 @@
 class FileParser
+  def to_integer(binary)
+    digits = binary.to_s.chars.map(&:to_i)
+    potentials = []
+    i = 0
+    digits.reverse_each do |element|
+      potentials << (element * (2**i)) 
+      i += 1
+    end
+    potentials.inject(:+)
+  end
+
+  def convert(path_name)
+    numbers = []
+    if File.exist? path_name
+        states_file = File.open(path_name)
+        while ! states_file.eof?
+          line = states_file.gets.chomp
+          numbers << to_integer(line)
+        end
+    end
+    numbers.inject(:+)
+  end
 end
