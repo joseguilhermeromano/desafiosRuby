8c1b2044eb99e8c56a985b2f1eda8ddbb2914189
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-19 02:33:24 +0000

    Finalização do desafio!

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,2 +1,25 @@
+require 'json'
+
 class Recipe
+
+  attr_accessor :title, :description, :ingredients, 
+  :cook_time, :featured
+
+  def initialize(properties)
+    @title = properties[:title]
+    @description = properties[:description]
+    @ingredients = properties[:ingredients]
+    @cook_time = properties[:cook_time]
+    @featured = properties[:featured]
+  end
+
+  def self.from_json(file)
+    if File.exist? file
+        data = File.read(file)
+        data_hash = JSON.parse(data,{:symbolize_names => true})
+        object = Recipe.new(data_hash)
+    end
+    object
+  end
+
 end
