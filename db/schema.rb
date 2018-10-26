58577b8048623e0104dfc3a28012a5f7c0583c9e
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-26 01:22:08 +0000

    Finalização do desafio!

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_25_025955) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "recipe_type"
+    t.string "cuisine"
+    t.string "difficulty"
+    t.string "cook_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
