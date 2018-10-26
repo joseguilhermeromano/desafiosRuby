258784b9376c2563762ba715c94c85161b87fd96
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-26 02:32:52 +0000

    Finalização do desafio!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def recipe_details
+    @recipe = Recipe.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/recipes/details/:id', to: 'home#recipe_details', as: 'details_recipe'
 end
