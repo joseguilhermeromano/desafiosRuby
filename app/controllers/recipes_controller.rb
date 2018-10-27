6a4c8cac7fd17868925d69c0b96f53e7ebf9331e
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-27 02:37:58 +0000

    Finalização do desafio!

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,16 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(params.require(:recipe).permit(:title, :cuisine, :recipe_type, 
+                                                        :difficulty, :cook_time, :ingredients,
+                                                        :cook_method))
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
 end
