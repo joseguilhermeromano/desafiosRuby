7c2cfe47b78c9cb52db712ffdc3814c6b8bec9b3
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-29 00:21:49 +0000

    Finalização do desafio!

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,5 @@
 class Cuisine < ApplicationRecord
+  validates :name, presence: { message: 'Você deve informar o nome da'\
+                                        ' cozinha' }
   has_many :recipes
 end
