68a85426dcb89b8c3727e6de577f7ebb8b7c87e5
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2018-10-30 22:41:06 +0000

    finalização do desafio!

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,7 +1,15 @@
 class Arrays
+
   def self.converte_impares_por(lista, numero)
+    array = []
+    array << lista.reject{|num| num.even?}
+    array << array[0].map{|num| num *= numero}
   end
 
   def self.converte_pares_por(lista, numero)
+    array = []
+    array << lista.reject{|num| num.odd?}
+    array << array[0].map{|num| num *= numero}
   end
+
 end
